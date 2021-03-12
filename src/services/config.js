d8fc81292519f0bf1b1ec435deabc674d11f3e7d
nca49 <vladshining@gmail.com>
2021-03-12 12:07:57 +0300

    library add

---
+++ src/services/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDaL8C6EU2jpNYvS2KDOqYVw-G_HD1MfVE",
+    authDomain: "agenda-49.firebaseapp.com",
+    projectId: "agenda-49",
+    storageBucket: "agenda-49.appspot.com",
+    messagingSenderId: "933532459792",
+    appId: "1:933532459792:web:30a21ba01d91799284bb95"
+  };
+firebase.initializeApp(firebaseConfig);
