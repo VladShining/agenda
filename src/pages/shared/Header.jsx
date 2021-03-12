3bc18bb4477ac1423d7ddb877472725a7eec477d
nca49 <vladshining@gmail.com>
2021-03-12 09:51:37 +0300

    firestore-config

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Clock from '../../components/Clock';
 
 const Header = ()=> {
-    return <h1>header</h1>
+    return <div className="headers"><h1>header</h1>
+             <Clock/>
+    </div>
+
 }
 
 export default Header;
